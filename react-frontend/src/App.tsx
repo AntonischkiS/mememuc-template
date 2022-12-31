e5380d7ef376810c800be326aa6b9f08f6e091a5
Simon Antonischki <s.antonischki@campus.lmu.de>
2022-12-31 14:07:06 +0100

    Convert App and index to tsx

---
+++ react-frontend/src/App.tsx
@@ -0,0 +1,35 @@
+// import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Profile from "./components/profile";
+import NoPage from "./components/NoPage";
+import Layout from  "./components/Layout";
+import * as React from "react";
+
+
+// function App() {
+//   return (
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Layout />}>
+//             <Route index element={<Profile />} />
+//             <Route path="*" element={<NoPage />} />
+//           </Route>
+//         </Routes>
+//       </BrowserRouter>
+//   );
+// }
+
+const App: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Profile />} />
+                    <Route path="*" element={<NoPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+export default App;
