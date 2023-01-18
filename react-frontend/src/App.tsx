9e273aaf39d9c4e88460a0c2dae35046ba650225
Simon Antonischki <s.antonischki@campus.lmu.de>
2023-01-18 18:03:21 +0100

    Add editor as route (still wip)

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -4,8 +4,11 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Profile from "./components/profile/Profile";
 import Layout from "./components/layout/Layout";
 import Login from "./components/login/Login";
+import Editor from "./components/editor";
 import * as React from "react";
 import Overview from "./components/overview/Overview";
+import Register from "./components/register/Register";
+import NoPage from "./components/NoPage";
 
 
 // function App() {
@@ -28,9 +31,11 @@ const App: React.FC = () => {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Overview />} />
                     <Route path="profile" element={<Profile />} />
-                    {/*<Route path={"editor"} element={}></Route>*/}
+                    <Route path={"editor"} element={Editor}></Route>
                 </Route>
                 <Route path="login" element={<Login />}  />
+                <Route path ="register" element={<Register/>} />
+                <Route path="*" element={<NoPage />} />
             </Routes>
         </BrowserRouter>
     );
