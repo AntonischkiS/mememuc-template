67c9aee844fe095c51fe4d257fa9a9658f584b03
Simon Antonischki <s.antonischki@campus.lmu.de>
2023-01-26 16:23:35 +0100

    Write in user in db when logging in to test db (not yet functional)

--- node-backend/routes/users.js
+++ node-backend/routes/users.js
@@ -1,16 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const {User} = require("../database/model");
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   const db = req.db;
+  console.log("Connected to database")
   const users = db.get('users');
   users.find({username: req.username},{ projection: {basicauthtoken: 0} }) // return all user properties, except the basic auth token
       .then((docs) => res.json(docs))
       .catch((e) => res.status(500).send())
 });
 router.post('/users', function(req, res, next) {
-  console.log(req.body);
-  res.status(200);
+  const db = req.db;
+  const users = db.get('users');
+  const u = new User(req.body);
+  users.insert(u); // save the user to the database when logging in. This is only to test the database. TODO: still not working
+
+  res.status(200).send();
+  users.find({username: req.username},{ projection: {basicauthtoken: 0} }) // return all user properties, except the basic auth token
+      .then((docs) => {
+        console.log(docs + " User inserted");
+        res.json(docs)})
+      .catch((e) => res.status(500).send())
 })
 module.exports = router;
