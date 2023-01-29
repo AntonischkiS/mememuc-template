c39f7062cd3dc4d2b20eb0c7a5ecdc11cf24e8f9
Simon Antonischki <s.antonischki@campus.lmu.de>
2023-01-29 17:30:06 +0100

    Changes to SingleView: - Refactor - Add simple function to add comments

--- react-frontend/src/components/singleview/SingleView.js
+++ react-frontend/src/components/singleview/SingleView.js
@@ -9,64 +9,79 @@ import SkipNextIcon from '@mui/icons-material/SkipNext';
 import CommentIcon from '@mui/icons-material/Comment';
 
 // import "../layout/Layout.css";
+import "./SingleView.css"
 
 function SingleView(navigation) {
     let {id} = useParams();
-    let id_next = Number(id) + 1
-    let id_prev = Number(id) - 1
+    let id_next = Number(id) + 1;
+    let id_prev = Number(id) - 1;
 
     // ToDO: 1000 must be replaced my last id
     let id_rnd = Math.floor(Math.random() * 1000);
-    let src = "".concat('https://picsum.photos/id/', id, '/5000/3333')
+    let src = "".concat('https://picsum.photos/id/', id, '/5000/3333');
     const navigate = useNavigate();
 
+    /**
+     * First idea for adding comments
+     * ! Doesn't keep comments to one meme
+     * ! better to store them in the database
+     */
+    function addComment(text) {
+        const ul = document.getElementById('commentUl');
+        const li = document.createElement('commentLi');
+        li.appendChild(document.createTextNode(text));
+        ul.appendChild(li);
+    }
+
     return (
-        <><>
+        <>
             <Card>
                 <CardContent>
+                    {/*Consider using non-static values like rem*/}
                     <LazyLoadImage
                         alt={"some meme"}
-                        height={768}
+                        className={"singleMeme"}
                         src={src}
-                        width={1024}/>
+                    />
                 </CardContent>
                 <CardActions>
                     <IconButton aria-label="prev" disabled={id_prev <= 0} size="small" color="primary"
-                                onClick={() => {
-                                    navigate(`/meme/${id_prev}`)
-                                }}>
+                                onClick={() => navigate(`/meme/${id_prev}`)}>
                         <SkipPreviousIcon/>
                     </IconButton>
-                    <IconButton aria-label="next" size="small" color="primary" onClick={() => {
-                        navigate(`/meme/${id_next}`)
-                    }}>
+                    <IconButton aria-label="next" size="small" color="primary"
+                                onClick={() => navigate(`/meme/${id_next}`)}>
                         <SkipNextIcon/>
                     </IconButton>
 
-                    <IconButton aria-label="next" size="small" color="primary" onClick={() => {
-                        navigate(`/meme/${id_next}`)
-                    }}>
+                    {/*Why navigate on this icon? Or rather do we need this icon? */}
+                    <IconButton aria-label="next" size="small" color="primary"
+                                onClick={() => navigate(`/meme/${id_next}`)}>
                         <CommentIcon/>
                     </IconButton>
 
-                    <Button size="small" color="primary" onClick={() => {
-                        navigate(`/meme/${id_rnd}`)
-                    }}>
+                    <Button size="small" color="primary"
+                            onClick={() => navigate(`/meme/${id_rnd}`)}>
                         Random Image
                     </Button>
-
                 </CardActions>
-                <TextField
 
+                {/*Text is submitted on enter, better to add comment button? */}
+                <TextField
                     id="outlined-multiline-static"
                     label="Multiline"
                     multiline
                     fullWidth
                     rows={4}
                     defaultValue="Default Value"
+                    onKeyDown={(event) => {
+                        if (event.key === "Enter") {
+                            addComment(event.target.value);
+                        }
+                    }}
                 />
+                <ul id ={"commentUl"} className={"commentUl"}></ul>
             </Card>
-        </>
         </>
     );
 }
